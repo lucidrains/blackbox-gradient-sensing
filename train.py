4dd97532d8762af527b10668ff5f48218b8f840d
lucidrains <lucidrains@gmail.com>
2025-05-05 03:52:02 +0000

    sort of learns

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.nn import Module, ModuleList
 from torch.distributions import Categorical
 from torch.func import functional_call
 
-from einops import reduce, rearrange, einsum
+from einops import reduce, rearrange, einsum, pack, unpack
 
 from tqdm import tqdm
 
@@ -31,6 +31,9 @@ def default(v, d):
 
 def divisible_by(num, den):
     return (num % den) == 0
+
+def is_empty(t):
+    return t.numel() == 0
 
 # mlp
 
@@ -118,12 +121,14 @@ class Actor(Module):
 def main(
     env_name = 'LunarLander-v3',
     total_learning_updates = 1000,
-    noise_pop_size = 50,
-    topk_elites = 4,
-    num_rollouts_before_update = 10,
-    learning_rate = 1e-1,
-    max_timesteps = 500,
-    actor_hidden_dim = 64,
+    noise_pop_size = 20,
+    topk_elites = 5,
+    num_rollouts_before_update = 2,
+    learning_rate = 1e-3,
+    weight_decay_strength = 0.1,
+    max_timesteps = 250,
+    actor_hidden_dim = 32,
+    noise_scale = 1.,
     seed = None,
     render = True,
     clear_videos = True,
@@ -137,7 +142,7 @@ def main(
         if clear_videos:
             rmtree(video_folder, ignore_errors = True)
 
-        total_eps_before_update = noise_pop_size * 2 * num_rollouts_before_update
+        total_eps_before_update = (noise_pop_size + 1) * 2 * num_rollouts_before_update
 
         env = gym.wrappers.RecordVideo(
             env = env,
@@ -171,28 +176,42 @@ def main(
 
         # keep track of the rewards received per noise and its negative
 
-        noises = []
-        reward_stats = torch.zeros((noise_pop_size, 2, num_rollouts_before_update)).to(device)
+        reward_stats = torch.zeros((noise_pop_size + 1, 2, num_rollouts_before_update)).to(device)
 
-        for noise_index in tqdm(range(noise_pop_size), desc = 'noise index', position = 1, leave = False):
+        episode_seeds = torch.randint(0, int(1e7), (num_rollouts_before_update,))
 
-            noise = {name: torch.randn_like(param) for name, param in params.items()}
-            episode_seeds = torch.randint(0, int(1e7), (num_rollouts_before_update,))
+        # create noises upfront
+
+        noises = dict()
+
+        for key, param in params.items():
+            noises_for_param = torch.randn((noise_pop_size + 1, *param.shape), device = device)
+
+            noises_for_param, ps = pack([noises_for_param], 'n *')
+            nn.init.orthogonal_(noises_for_param)
+            noises_for_param, = unpack(noises_for_param, ps, 'n *')
+
+            noises_for_param[0].zero_() # first one is for baseline
+            noises[key] = noises_for_param
+
+        for noise_index in tqdm(range(noise_pop_size + 1), desc = 'noise index', position = 1, leave = False):
+
+            noise = {key: noises_for_param[noise_index] for key, noises_for_param in noises.items()}
 
             for sign_index, sign in tqdm(enumerate((1, -1)), desc = 'sign', position = 2, leave = False):
 
-                param_with_noise = {name: (noise[name] * sign + params[name]) for name in params}
+                param_with_noise = {name: (noise[name] * sign + param) for name, param in params.items()}
 
                 for episode_index in tqdm(range(num_rollouts_before_update), desc = 'episodes', position = 3, leave = False):
 
                     state, _ = env.reset(seed = episode_seeds[episode_index].item())
-                    state = torch.from_numpy(state).to(device)
 
                     total_reward = 0.
 
                     for timestep in range(max_timesteps):
 
                         with torch.inference_mode():
+                            state = torch.from_numpy(state).to(device)
                             action_probs = functional_call(actor, param_with_noise, state)
 
                         dist = Categorical(action_probs)
@@ -207,46 +226,75 @@ def main(
 
                         if done:
                             break
+
+                        state = next_state
                 
                     reward_stats[noise_index, sign_index, episode_index] = total_reward
 
-            noises.append(noise)
+        # remove the baseline noise
 
-        # stack all the noises
-
-        param_names = params.keys()
-        stacked_noises = [torch.stack(tensors) for tensors in zip(*[noise.values() for noise in noises])]
-
-        stacked_noises = dict(zip(param_names, stacked_noises))
+        noises = {key: noises_for_param[1:] for key, noises_for_param in noises.items()}
 
         # update based on eq (3) and (4) in the paper
         # their contribution is basically to use reward deltas (for a given noise and its negative sign) for ranking the top perturbations
 
         # n - noise, s - sign, e - episode
 
+        noise_indices = torch.arange(noise_pop_size, device = reward_stats.device)
+
+        baseline, reward_stats = reward_stats[0], reward_stats[1:]
+
+        reward_std = reward_stats.std()
+
         reward_mean = reduce(reward_stats, 'n s e -> n s', 'mean')
+
+        should_update = (reward_mean > torch.median(baseline)).any(dim = -1)
+
+        noise_indices = noise_indices[should_update]
+
+        reward_mean = reward_mean[should_update]
+
+        if is_empty(reward_mean):
+            learning_updates_pbar.set_description(f'no rewards past baseline {baseline.mean().item():.3f}')
+            continue
+
+        num_rewards_greater_than_baseline = reward_mean.shape[0]
 
         reward_deltas = reward_mean[:, 0] - reward_mean[:, 1]
 
         # get the topk elite indices
 
-        ranked_reward_deltas, ranked_reward_indices = reward_deltas.topk(topk_elites, dim = 0)
+        ranked_reward_deltas, ranked_reward_indices = reward_deltas.abs().topk(min(topk_elites, num_rewards_greater_than_baseline), dim = 0)
 
         # get the weights for the weighted sum of the topk noise according to eq (3)
 
-        reward_std = reward_mean[ranked_reward_indices].std()
-        weights = (ranked_reward_deltas / reward_std.clamp(min = 1e-3)) * learning_rate
+        weights = ranked_reward_deltas / reward_std.clamp(min = 1e-3) * learning_rate
+
+        # modulate the weights by sign and whether the reward for pos/neg noise is greater than baseline or not
+
+        delta_signs = reward_deltas[ranked_reward_indices].sign() # since using absolute value
+
+        weights *= delta_signs
 
         # update the param one by one
 
-        for param, stacked_noise in zip(params.values(), stacked_noises.values()):
-            elite_noises = stacked_noise[ranked_reward_indices]
+        sel_noise_indices = noise_indices[ranked_reward_indices]
 
-            update = einsum(elite_noises, weights, 'n ..., n -> ...')
+        for param, noise in zip(params.values(), noises.values()):
+
+            best_noises = noise[sel_noise_indices]
+
+            update = einsum(best_noises, weights, 'n ..., n -> ...')
 
             param.data.add_(update)
 
-        learning_updates_pbar.set_description(f'best: {reward_mean.amax().item():.2f} | best delta: {ranked_reward_deltas.amax().item():.2f}')
+            # weight decay
+
+            param.norm(p = 1).backward()
+            param.data.sub_(param.grad * learning_rate * weight_decay_strength)
+            param.grad = None
+
+        learning_updates_pbar.set_description(f'best: {reward_mean.amax().item():.2f} | best delta: {ranked_reward_deltas.amax().item():.2f} | {num_rewards_greater_than_baseline} rewards past baseline')
 
 if __name__ == '__main__':
     fire.Fire(main)
